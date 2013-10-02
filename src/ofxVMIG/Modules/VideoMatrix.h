55ceb5c428b040b52449d78a1c4bdb574facc362
unknown <elliot@kimchiandchips.com>
2013-10-02 12:07:29 +0100

    first commit

---
+++ src/ofxVMIG/Modules/VideoMatrix.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "ofxVMIG/Module.h"
+
+namespace ofxVMIG {
+	namespace Modules {
+		class VideoMatrix : public Module {
+			void setup() override;
+			void update() override;
+			void draw(float width, float height) override;
+			void populateInspector(ofxUICanvas &canvas) override;
+			void inspectorCallback(ofxUIEventArgs &args) override;
+		};
+	}
+}
